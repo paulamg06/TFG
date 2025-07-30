f80581048554f75ea72965b313dc808c127e140d
paulamg06 <100472240@alumnos.uc3m.es>
2025-07-30 11:28:41 +0200

    Modificado selector para el lenguaje de Python, incluyendo el nombre del método

--- cbomkit/frontend/src/helpers/rules-manager.js
+++ cbomkit/frontend/src/helpers/rules-manager.js
@@ -7,7 +7,7 @@ con la carga de activos que provienen de los inventarios de reglas
 tanto en Python (python_rules.json) como en Java (java_rules.json) */
 
 // Función que construye el árbol de reglas teniendo en cuenta las siguientes longitudes
-function buildRuleTree(tree, nodeList, father) {
+function buildRuleTree(tree, nodeList, father, language) {
   const actualNode = nodeList[0].trim();
   let idName = "";
   
@@ -30,9 +30,14 @@ function buildRuleTree(tree, nodeList, father) {
     // Llamamos a la función de forma iterativa pasandole la lista de hijos de ese id
     const subTree = tree.find(node => node.id === idName);
     if (subTree["children"]) {
-      buildRuleTree(subTree["children"], nodeList.slice(1), subTree.id);
+      buildRuleTree(subTree["children"], nodeList.slice(1), subTree.id, language);
     }
   } else { // Nodo hoja
+    // Versión 3: si el lenguaje es python, se le añade al nodo hoja el sufijo
+    // _method para aplicarlo en la detección en el plugin
+    if (language === 'python') {
+      idName += `_method`
+    }
     if (!tree.some(child => child.id === idName)) { // Si no existe, lo añadimos
       tree.push({
         id: idName,
@@ -42,24 +47,39 @@ function buildRuleTree(tree, nodeList, father) {
   }
 }
 
-// Función que lee los .json con las reglas y devuelve una lista con ellas separadas en función del id
-function getAllJsonRules(language) {
-  let file = null;
-  // Añadir aqui futuros lenguajes
-  if (language === 'python') {
-    file = pythonRulesData;
-  }
-  else {
-    file = javaRulesData;
-  }
-
+// Función que lee  java_rules.json con las reglas y 
+// devuelve una lista con ellas separadas en función del id
+function getRulesListFromJson(language) {
   let rulesList = [];
 
+  let jsonFile = null;
+
+  if (language === 'python') {
+    jsonFile = pythonRulesData;
+  } // Añadir aqui futuros lenguajes
+  else {
+    jsonFile = javaRulesData;
+  }
+
   // Iteramos por cada regla
-  file.forEach(rule => {
+  jsonFile.forEach(rule => {
     // Nos quedamos con el primer elemento del id
     const id = rule.id.split('|')[0];
     const groups = id.split('.');
+
+    // Versión 3: en caso de que el lenguaje sea Python, vamos
+    // a escoger también el methodName, que lo añadiremos al final
+    if (language === 'python') {
+      const methodName = rule.methodMatcher.methodNames[0];
+
+      // Si es mayor de 4, omitimos el último elemento
+      if (groups.length > 4) {
+        groups.pop();
+      }
+
+      // Añadimos el nombre del método al final de la lista
+      groups.push(methodName);
+    }
 
     rulesList.push(groups);
   });
@@ -70,12 +90,14 @@ function getAllJsonRules(language) {
 // Función para procesar los activos de los json según el lenguage seleccionado
 export function processAssets(language) {
   const auxAssetsTree = [];
-  const rulesList = getAllJsonRules(language);
+
+  const rulesList = getRulesListFromJson(language);
 
   // Construioms el árbol iterando por cada lista
-  rulesList.forEach(rule => buildRuleTree(auxAssetsTree, rule, ""));
+  rulesList.forEach(rule => buildRuleTree(auxAssetsTree, rule, "", language));
 
   console.log(`tree for language ${language}: `, auxAssetsTree);
 
   return auxAssetsTree;
 }
+
